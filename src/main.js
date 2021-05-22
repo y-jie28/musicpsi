4acb5ff0e625a8aa26e8207c1179514f69b7c36d
Yiying Jie <yiying.j@outlook.com>
2021-05-21 17:22:22 -0700

    splitting the app into smaller pieces

--- src/main.js
+++ src/main.js
@@ -5,4 +5,12 @@ import store from './store';
 import './assets/tailwind.css';
 import './assets/main.css';
 
-createApp(App).use(store).use(router).mount('#app');
+// createApp(App).use(store).use(router).mount('#app');
+
+// it makes the code more readable after breaking into separate lines
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
