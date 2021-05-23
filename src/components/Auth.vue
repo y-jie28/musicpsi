942ec58cd87e22586bfac71de1e6113cfeee0b0d
Yiying Jie <yiying.j@outlook.com>
2021-05-22 18:51:36 -0700

    toggle modal and closing modal

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Auth Modal -->
-  <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+  <div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="{ hidden: !authModalShow }">
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
@@ -22,7 +22,7 @@
           <div class="flex justify-between items-center pb-4">
             <p class="text-2xl font-bold">Your Account</p>
             <!-- Modal Close Button -->
-            <div class="modal-close cursor-pointer z-50">
+            <div class="modal-close cursor-pointer z-50" @click.prevent="toggleAuthModal">
               <i class="fas fa-times"></i>
             </div>
           </div>
@@ -149,7 +149,17 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'Auth',
+  computed: {
+    authModalShow() {
+      return this.$store.getters.authModalShow;
+    },
+  },
+  methods: {
+    ...mapMutations(['toggleAuthModal']),
+  },
 };
 </script>
